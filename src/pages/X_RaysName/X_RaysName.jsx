631071a98abe719603adf7468e864068948ae769
GalalElaasar9 <138381852+GalalElaasar9@users.noreply.github.com>
2024-06-24 00:20:00 +0300

    .

--- src/pages/X_RaysName/X_RaysName.jsx
+++ src/pages/X_RaysName/X_RaysName.jsx
@@ -1,15 +1,42 @@
 import './X_RaysName.css';
-
+import ImgXRayes_1 from '../../assets/images/x-ray1.png'
+import ImgXRayes_2 from '../../assets/images/x-ray2.png'
+import ImgXRayes_3 from '../../assets/images/x-ray3.png'
 const X_RaysName = () => {
   return (
     <div className='X_RaysName'>
       <div className="container">
-        <div className="heading text-center">
+        <div className="heading text-center pt-4">
           <h2>X_Ray Name</h2>
         </div>
-        <div className="imagesName d-flex justify-content-start align-items-center gap-2 flex-wrap">
+        <div className="imagesName mt-4 d-flex justify-content-start align-items-center gap-3 flex-wrap">
           <div className="imageName">
-            
+            <img src={ImgXRayes_1} alt="" />
+            <h6 className='mt-3 mb-0 text-center'>X_Ray Name</h6>
+          </div>
+          <div className="imageName">
+            <img src={ImgXRayes_2} alt="" />
+            <h6 className='mt-3 mb-0 text-center'>X_Ray Name</h6>
+          </div>
+          <div className="imageName">
+            <img src={ImgXRayes_3} alt="" />
+            <h6 className='mt-3 mb-0 text-center'>X_Ray Name</h6>
+          </div>
+          <div className="imageName">
+            <img src={ImgXRayes_1} alt="" />
+            <h6 className='mt-3 mb-0 text-center'>X_Ray Name</h6>
+          </div>
+          <div className="imageName">
+            <img src={ImgXRayes_2} alt="" />
+            <h6 className='mt-3 mb-0 text-center'>X_Ray Name</h6>
+          </div>
+          <div className="imageName">
+            <img src={ImgXRayes_3} alt="" />
+            <h6 className='mt-3 mb-0 text-center'>X_Ray Name</h6>
+          </div>
+          <div className="imageName">
+            <img src={ImgXRayes_1} alt="" />
+            <h6 className='mt-3 mb-0 text-center'>X_Ray Name</h6>
           </div>
         </div>
       </div>
@@ -17,4 +44,4 @@ const X_RaysName = () => {
   )
 }
 
-export default X_RaysName
+export default X_RaysName;
