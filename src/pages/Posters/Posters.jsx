c6c4bac1ae419cebc99f231620b7efbe9b9bcb1a
GalalElaasar9 <138381852+GalalElaasar9@users.noreply.github.com>
2024-06-24 01:17:13 +0300

    .

--- src/pages/Posters/Posters.jsx
+++ src/pages/Posters/Posters.jsx
@@ -1,57 +1,41 @@
 import './Posters.css'
 import { CiHeart } from "react-icons/ci";
+import { useState } from 'react';
 import poster from '../../assets/images/adbdecfb6a9c4b872702d8a52799d6e5.jpg'
+
 const Posters = () => {
+  // حالة لكل مربع
+  const [liked, setLiked] = useState(Array(6).fill(false));
+
+  const handleDoubleClick = (index) => {
+    setLiked(prevLiked => {
+      const newLiked = [...prevLiked];
+      newLiked[index] = !newLiked[index];
+      return newLiked;
+    });
+  };
+
   return (
     <div className='posters'>
       <div className="container">
         <div className="boxes d-flex justify-content-start align-items-center flex-wrap gap-3">
-          <div className="box">
-            <img src={poster} alt="" />
-            <p>Vita</p>
-            <div className="iconHeart">
-              <CiHeart/>
+          {Array.from({ length: 6 }).map((_, index) => (
+            <div 
+              className="box" 
+              key={index} 
+              onDoubleClick={() => handleDoubleClick(index)}
+            >
+              <img src={poster} alt="" />
+              <p>Vita</p>
+              <div className="iconHeart">
+                <CiHeart style={{ color: liked[index] ? 'red' : 'black' }} />
+              </div>
             </div>
-          </div>
-          <div className="box">
-            <img src={poster} alt="" />
-            <p>Vita</p>
-            <div className="iconHeart">
-              <CiHeart/>
-            </div>
-          </div>
-          <div className="box">
-            <img src={poster} alt="" />
-            <p>Vita</p>
-            <div className="iconHeart">
-              <CiHeart/>
-            </div>
-          </div>
-          <div className="box">
-            <img src={poster} alt="" />
-            <p>Vita</p>
-            <div className="iconHeart">
-              <CiHeart/>
-            </div>
-          </div>
-          <div className="box">
-            <img src={poster} alt="" />
-            <p>Vita</p>
-            <div className="iconHeart">
-              <CiHeart/>
-            </div>
-          </div>
-          <div className="box">
-            <img src={poster} alt="" />
-            <p>Vita</p>
-            <div className="iconHeart">
-              <CiHeart/>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
   )
 }
 
-export default Posters
+export default Posters;
