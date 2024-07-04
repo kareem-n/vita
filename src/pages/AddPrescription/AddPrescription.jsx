d954772a98ec8812bdbf11e342d4f008a843ad96
GalalElaasar9 <138381852+GalalElaasar9@users.noreply.github.com>
2024-07-05 01:44:17 +0300

    ..

--- src/pages/AddPrescription/AddPrescription.jsx
+++ src/pages/AddPrescription/AddPrescription.jsx
@@ -3,7 +3,50 @@ import './AddPrescription.css'
 import { IoAddOutline } from "react-icons/io5";
 
 const AddPrescription = () => {
-  
+  const [dataMedicine, setDataMedicine] = useState([{
+    medicine:"",
+    note:""
+  }])
+
+  const handleClickMedicine =()=>{
+    setDataMedicine([...dataMedicine,{medicine:"",note:""}])
+  }
+  const handleChangeMedicine = (e,i) =>{
+    const {name,value} = e.target;
+    const onchangeValue = [...dataMedicine];
+    onchangeValue[i][name] = value;
+    setDataMedicine(onchangeValue)
+  }
+
+  const [dataTest, setDataTest] = useState([{
+    test:"",
+    note:""
+  }])
+
+  const handleClickTest =()=>{
+    setDataTest([...dataTest,{test:"",note:""}])
+  }
+  const handleChangeTest = (e,i) =>{
+    const {name,value} = e.target
+    const onchangeValue = [...dataTest];
+    onchangeValue[i][name] = value;
+    setDataTest(onchangeValue)
+  }
+
+  const [dataXRay, setDataXRay] = useState([{
+    x_ray:"",
+    note:""
+  }])
+
+  const handleClickXRay =()=>{
+    setDataXRay([...dataXRay,{x_ray:"",note:""}])
+  }
+  const handleChangeXRay = (e,i) =>{
+    const {name,value} = e.target
+    const onchangeValue = [...dataTest];
+    onchangeValue[i][name] = value;
+    setDataXRay(onchangeValue)
+  }
 
   return (
     <form action='' className='AddPrescription'>
@@ -12,14 +55,15 @@ const AddPrescription = () => {
             <h3>Medicine</h3>
             <h3>Note</h3>
           </div>
-          
-            <div className='inputs'>
-                <input type="text" name='medicine'  required placeholder='Medicine'/>
-                <input type="text" name='note' required placeholder='Note'/>
+          {
+            dataMedicine.map((value,i)=>
+            <div key={i} className='inputs'>
+                <input type="text" name='medicine' value={value.medicine} onChange={(e)=>handleChangeMedicine(e,i)} required placeholder='Medicine'/>
+                <input type="text" name='note' value={value.note} onChange={(e)=>handleChangeMedicine(e,i)} required placeholder='Note'/>
               </div>
-            
-          
-            <button type='button' className='addBtn'>
+            )
+          }
+            <button type='button' className='addBtn' onClick={handleClickMedicine}>
               <IoAddOutline/>
             </button>
         {/* <p>{JSON.stringify(dataMedicine)}</p> */}
@@ -30,13 +74,15 @@ const AddPrescription = () => {
           <h3>Test</h3>
           <h3>Note</h3>
         </div>
-        
+        {
+          dataTest.map((value,i)=>
           <div key={i} className='inputs'>
-              <input type="text" name='test'  placeholder='Test' required/>
-              <input type="text" name='note'  placeholder='Note' required/>
+              <input type="text" name='test' value={value.test} onChange={(e)=>handleChangeTest(e,i)} placeholder='Test' required/>
+              <input type="text" name='note' value={value.note} onChange={(e)=>handleChangeTest(e,i)} placeholder='Note' required/>
             </div>
-          
-        <button type='button' className='addBtn' >
+          )
+        }
+        <button type='button' className='addBtn' onClick={handleClickTest}>
           <IoAddOutline/>
         </button>
       {/* <p>{JSON.stringify(dataTest)}</p> */}
@@ -47,14 +93,15 @@ const AddPrescription = () => {
           <h3>X_Ray</h3>
           <h3>Note</h3>
         </div>
-       
+        {
+          dataXRay.map((value,i)=>
             <div key={i} className='inputs'>
-              <input type="text" name='test'  required placeholder='X_Ray'/>
-              <input type="text" name='note'  required placeholder='Note'/>
+              <input type="text" name='test' value={value.x_ray} onChange={(e)=>handleChangeXRay(e,i)} required placeholder='X_Ray'/>
+              <input type="text" name='note' value={value.note} onChange={(e)=>handleChangeXRay(e,i)} required placeholder='Note'/>
             </div>
-          
-        
-        <button type='button' className='addBtn'>
+          )
+        }
+        <button type='button' className='addBtn' onClick={handleClickXRay}>
           <IoAddOutline/>
         </button>
       {/* <p>{JSON.stringify(dataXRay)}</p> */}
